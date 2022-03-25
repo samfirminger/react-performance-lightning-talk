26d1c59d531377950c0bed434b1f27d54bee06f6
Sam Firminger <samfirminger@gmail.com>
2022-03-25 13:00:44 +0000

    added profiler and long expsneive calculation

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -8,17 +8,39 @@ const ChildContainer = styled(Container)`
   background-color: #b3dec1;
 `;
 
+const Content = styled.div`
+  display: block;
+
+  &:first-child {
+    margin-bottom: 20px;
+  }
+`;
+
 const Child = () => {
   const renderCount = useRef(1);
   useEffect(() => {
     renderCount.current++;
   });
 
+  const doLongAndExpensiveCalculation = (size: number) => {
+    const array = [];
+    for (let i = 0; i < size; i++) {
+      array.push(i);
+    }
+    return array[size - 1];
+  };
+
   return (
     <div>
       <ChildContainer>
-        I am a functional component child and I've rendered{" "}
-        {renderCount.current} time(s)
+        <Content>
+          I am a functional component child and I've rendered{" "}
+          {renderCount.current} time(s)
+        </Content>
+        <Content>
+          The result of my expensive calc is:{" "}
+          {doLongAndExpensiveCalculation(10000000)}
+        </Content>
       </ChildContainer>
       <GrandChild />
     </div>
--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { Profiler, useState } from "react";
 import styled from "styled-components";
 import Child from "./Child";
 import { Container } from "./Container";
 
 const ParentContent = styled(Container)`
   margin-top: 500px;
-  background-color: #E5FCF5;
+  background-color: #e5fcf5;
   color: #210124;
 `;
 
@@ -14,19 +14,37 @@ const Button = styled.button`
   border-radius: 4px;
   padding: 15px;
   cursor: pointer;
-`
+`;
 
 const Parent = () => {
   const [count, setCount] = useState(0);
 
+  const clockPerformance = (
+    profilerId: any,
+    mode: any,
+    actualTime: any,
+    baseTime: any,
+    startTime: any,
+    commitTime: any
+  ) => {
+    console.log({
+      mode,
+      actualTime,
+      baseTime,
+    });
+  };
+
   return (
-    <>
+    <Profiler
+      id={"test"}
+      onRender={clockPerformance}
+    >
       <ParentContent>
         <h1>I am the parent</h1>
         <Button onClick={() => setCount(count + 1)}>Click me!</Button>
       </ParentContent>
       <Child />
-    </>
+    </Profiler>
   );
 };
 
